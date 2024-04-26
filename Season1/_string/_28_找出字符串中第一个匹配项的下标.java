36203eae025feec7c7ccff92fd3f4c46a85fdcbe
Savannah <zgshighwin@gmail.com>
2024-04-26 09:47:11 +0800

    q_27 finish

---
+++ Season1/_string/_28_找出字符串中第一个匹配项的下标.java
@@ -0,0 +1,32 @@
+package _string;
+
+
+/**
+ * 给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串的第一个匹配项的下标（下标从 0 开始）。如果 needle 不是 haystack 的一部分，则返回  -1 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：haystack = "sadbutsad", needle = "sad"
+ * 输出：0
+ * 解释："sad" 在下标 0 和 6 处匹配。
+ * 第一个匹配项的下标是 0 ，所以返回 0 。
+ * 示例 2：
+ * <p>
+ * 输入：haystack = "leetcode", needle = "leeto"
+ * 输出：-1
+ * 解释："leeto" 没有在 "leetcode" 中出现，所以返回 -1 。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= haystack.length, needle.length <= 104
+ * haystack 和 needle 仅由小写英文字符组成
+ */
+public class _28_找出字符串中第一个匹配项的下标 {
+    public int strStr(String haystack, String needle) {
+
+        return 0;
+    }
+}
